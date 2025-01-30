f9db0eb3501f7d7a81730b086454996a72a83c20
Azhar Rizqullah <azharizqullah90@gmail.com>
2025-01-30 09:30:14 +0800

    refactor: small restructuring

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
+
 pub mod config;
+
+pub fn listen<T: ToSocketAddrs>(socket_addr: T) {
+    let listener = TcpListener::bind(socket_addr).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(stream: TcpStream) {
+    println!("Connection established.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use std::net::TcpListener;
-
 use clap::Parser;
 use wasabi::config::Config;
 
 fn main() {
     let config = Config::parse();
 
-    let listener = TcpListener::bind(("localhost", config.port)).unwrap();
     println!("Listening on port {}", config.port);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        println!("{:#?}", stream);
-        println!("Connection established!");
-    }
+    wasabi::listen(("localhost", config.port));
 }
