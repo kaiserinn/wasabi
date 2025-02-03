5b0003b962513f73aea8e1295e2e4196e4830f0a
Azhar Rizqullah <azharizqullah90@gmail.com>
2025-02-03 22:36:30 +0800

    feat: custom parser for `serve_dir`

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path, path::PathBuf};
 
 const DEFAULT_PORT: u16 = 7878;
 
@@ -7,5 +7,19 @@ pub struct Config {
     #[arg(short, long, env, default_value_t = DEFAULT_PORT)]
     pub port: u16,
 
-    pub serve_dir: Option<PathBuf>,
+    #[arg(value_parser = directory_parser, default_value = ".")]
+    pub serve_dir: PathBuf,
+}
+
+pub fn directory_parser(input: &str) -> Result<PathBuf, &'static str> {
+    let path = path::Path::new(input);
+
+    if !path.is_dir() {
+        return Err(
+            "Invalid path: Either does not exist or is not a directory",
+        );
+    }
+
+    path::Path::canonicalize(path)
+        .map_err(|_| "Failed to resolve absolute path")
 }
