93077a65663049db510b60c8c694d91e312f7933
Azhar Rizqullah <azharizqullah90@gmail.com>
2025-02-04 22:07:47 +0800

    refactor: `serve`

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,21 @@ use request::Request;
 pub mod config;
 pub mod request;
 
-pub fn listen<T: ToSocketAddrs>(socket_addr: T) {
-    let listener = TcpListener::bind(socket_addr).unwrap();
+pub fn serve(config: Config) {
+    let localhost: [u8; 4] = [127, 0, 0, 1];
+    let addrs = [
+        SocketAddr::from((localhost, config.port)),
+        SocketAddr::from((localhost, config.port + 1)),
+        SocketAddr::from((localhost, config.port + 2)),
+    ];
+    let listener =
+        TcpListener::bind(&addrs[..]).expect("Failed to bind to address");
+    println!("Listening on address {:?}", listener.local_addr().unwrap());
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        handle_connection(stream, &config.serve_dir);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,5 @@ use wasabi::config::Config;
 fn main() {
     let config = Config::parse();
 
-    println!("Listening on port {}", config.port);
-    wasabi::listen(("localhost", config.port));
+    wasabi::serve(config);
 }
